bd644bb0e420f59af60cb3641f370f4e7d38faec
samuelsanpo <33384700+samuelsanpo@users.noreply.github.com>
2018-10-08 17:18:43 -0500

    Samuel

--- Facade/src/Singleton/Client.java
+++ Facade/src/Singleton/Client.java
@@ -62,7 +62,77 @@ public class Client {
 			case 2:
 				correo = JOptionPane.showInputDialog("Introducir correo");
 				contrasena = JOptionPane.showInputDialog("Introducir contrasena");
-                                JOptionPane.showMessageDialog(null, ""+ ps.unicaInstancia(correo, contrasena));
+                                String a = JOptionPane.showInputDialog(null, ""+ ps.unicaInstancia(correo, contrasena));
+                                int b =  Integer.parseInt(a);
+                                switch(b){
+                                    case 1:
+                                        String nombrereserva =JOptionPane.showInputDialog("Nombre de reserva");
+                                        String nombrerutar = JOptionPane.showInputDialog("Nombre de ruta reservada");
+                                        String puestoruta =JOptionPane.showInputDialog("Puesto de la ruta");
+                                        int puestorutan = Integer.parseInt(puestoruta);
+                                        String documentop= JOptionPane.showInputDialog("Documento de pasajero");
+                                        
+                                        facade.crearRegistroReserva(nombrereserva, nombrerutar, puestorutan, documentop);
+                                        
+                                        break;
+                                        
+                                    case 2:
+                                        System.out.print("Hola");
+                                        String documentopa= JOptionPane.showInputDialog("Documento");
+                                        
+                                        int veri = facade.verificarUsuario(correo, contrasena);
+                                        
+                                        if (veri ==1){
+                                            
+                                            //PASAJERO
+                                            
+                                            facade.listarReservasPasajero(documentopa);
+                                        }else {
+                                            if(veri ==2){
+                                                //CONDUCTOR
+                                                
+                                               facade.listarRutasConductor(documentopa);
+                                            } else{
+                                                    if (veri ==0){
+                                                     JOptionPane.showMessageDialog(null, "No hay listas que mostrar");
+                                                    }
+                                                  }  
+                                               }
+                                        break;
+                                        
+                                    case 3:
+                                        String nombrereservam =JOptionPane.showInputDialog("Nombre de reserva");
+                                        String nombrerutarm = JOptionPane.showInputDialog("Nombre de ruta reservada");
+                                        String puestorutam =JOptionPane.showInputDialog("Puesto de la ruta");
+                                        int puestorutanm = Integer.parseInt(puestorutam);
+                                        String documentopm= JOptionPane.showInputDialog("Documento de pasajero");
+                                        int veri2 = facade.verificarUsuario(correo, contrasena);
+                                        
+                                        if (veri2 ==1){
+                                            
+                                            //PASAJERO
+                                            
+                                           facade.modificarReservaPasajero(nombrereservam, nombrereservam, puestorutanm, documentopm);
+                                        }else {
+                                            if(veri2 ==2){
+                                                //CONDUCTOR
+                                                
+                                               JOptionPane.showMessageDialog(null, "No hay reservas que modificar");
+                                            } else{
+                                                    if (veri2 ==0){
+                                                     JOptionPane.showMessageDialog(null, "No hay reservas que modificar");
+                                                    }
+                                                  }  
+                                               }
+                                        
+                                       break;
+                                       
+                                    case 4:
+                                        String nombrereservae =JOptionPane.showInputDialog("Nombre de reserva");
+                                        String documentope= JOptionPane.showInputDialog("Documento de pasajero");
+                                        
+                                       facade.eliminarReserva(nombrereservae, documentope);
+                                      }  
 				break;
 			}
 		} while (opcion != 0);
